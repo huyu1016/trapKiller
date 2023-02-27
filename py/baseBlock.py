ea6a13ac44f014739af052aec10912726e20e520
huyu1016 <126449021+huyu1016@users.noreply.github.com>
2023-02-27 15:18:12 +0800

    Add files via upload

---
+++ py/baseBlock.py
@@ -0,0 +1,33 @@
+class BasicBlock:
+    def __init__(self, start_address):
+        self.start = start_address
+        self.instructions = []  # each instruction is a string
+
+    def get_start_address(self):
+        return self.start
+    def set_end_address(self,end_address):
+        self.end = end_address
+    def get_end_address(self):
+        return  self.end
+    def set_fall_to(self,fall_to):
+        self.fall_to = fall_to
+    def get_fall_to(self):
+        return  self.fall_to
+    def add_instruction(self, instruction):
+        self.instructions.append(instruction)
+    def set_jump_to(self,jump_to):
+        self.jump_to = jump_to
+    def get_jump_to(self):
+        return  self.jump_to
+    def get_instructions(self):
+        return self.instructions
+
+    def set_block_type(self, type):
+        self.type = type
+
+    def get_block_type(self):
+        return self.type
+
+    def display(self):
+        print("================")
+
